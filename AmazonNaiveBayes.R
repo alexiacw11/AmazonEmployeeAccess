35189395b33ae0067d4a7ce56b8b233cb60f5ae1
Alexia W <130490875+alexiacw11@users.noreply.github.com>
2024-10-23 12:06:54 -0600

    Add files via upload

---
+++ AmazonNaiveBayes.R
@@ -0,0 +1,94 @@
+# Naive Bayes Classifier 
+
+# Load libraries
+library(ggplot2)
+library(ROSE)
+library(tidymodels)
+library(discrim)
+library(naivebayes)
+library(embed)
+tidymodels_prefer()
+
+# Read in the data 
+train <- vroom::vroom("train.csv")
+test <- vroom::vroom("test.csv")
+
+# Transform response to factor
+train <- train |> 
+  mutate(ACTION = as.factor(ACTION))
+
+# Balance the data with undersampling
+balanced_train <- ovun.sample(ACTION ~ ., data = train, method = "under")$data
+
+# Take 10% of the data 
+ten_perc <- balanced_train |> 
+  sample_frac(0.10)
+
+# Recipe
+my_recipe <- recipe(ACTION ~ ., data = ten_perc) |> 
+  step_mutate_at(all_nominal_predictors(), fn=factor) |> 
+  step_normalize(all_nominal_predictors())
+
+nb_model <- naive_Bayes(Laplace = tune(), smoothness = tune()) |> 
+  set_mode("classification") |> 
+  set_engine("naivebayes")
+
+nb_wf <- workflow() |> 
+  add_recipe(my_recipe) |> 
+  add_model(nb_model)
+
+# Grid of values to tune over
+grid_of_tuning_params <- grid_regular(Laplace(), smoothness(), levels = 5) # L^2 total tuning possibilties
+
+
+# Split data for CV (5-10 groups)
+folds <- vfold_cv(ten_perc, v=5, repeats = 1)
+
+# Run the CV
+CV_results <- nb_wf %>%
+  tune_grid(resamples=folds, 
+            grid=grid_of_tuning_params, 
+            metrics=metric_set(roc_auc))
+
+# Find best tuning parameters
+bestTune <- CV_results %>%
+  select_best(metric = "roc_auc")
+
+# Finalize the workflow and fit it
+final_wf <- nb_wf %>%
+  finalize_workflow(bestTune) %>% 
+  fit(data=ten_perc)
+
+# Make predictions
+predictions <- predict(final_wf, new_data = test, type = "prob")
+
+# Kaggle submission 
+kaggle_submission <- predictions %>%  
+  bind_cols(., test) %>% 
+  dplyr::select(id, .pred_1) %>% 
+  rename(ACTION= .pred_1)
+
+# 0.57637
+vroom::vroom_write(kaggle_submission, "NaiveBayesPreds.csv", delim = ",")
+
+# Here I included this line in my recipe - step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION))
+#0.57330, did worse
+vroom::vroom_write(kaggle_submission, "NaiveBayesUpdatedPreds.csv", delim = ",")
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+  
+  
+  
